7e0d1a1534522ae6a1298dfabe145dfc49440f8b
annalena-project <aschaeffer@northpark.edu>
2026-02-19 15:25:22 -0600

    added longitude and latitude

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 URL = " https://geocoding-api.open-meteo.com/v1/search"
 
 parameters = {
-    "name": "Toronto",
+    "name": "Toronto",      # name[city]
     "country": "Canada",
     "count": 1
 }
@@ -12,3 +12,15 @@ response = requests.get(URL, params = parameters, timeout = 10)
 
 print(response.status_code)
 print(response.json())
+
+data = response.json()
+
+result = data["results"]
+first_result = result[0]
+
+latitude = first_result["latitude"]
+longitude = first_result["longitude"]
+
+print("latitude:", latitude)
+print("longitude:", longitude)
+
