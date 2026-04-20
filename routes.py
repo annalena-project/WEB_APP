396d0149f8f0b9c8f6af75cba91eb671635d2a6a
annalena-project <aschaeffer@northpark.edu>
2026-04-20 16:16:38 -0500

    Updated routes with  FLASK CRUD

--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
 
-from flask import Flask 
+from flask import Flask
+
+from populate import DatabaseManager
+db = DatabaseManager()
 
 app = Flask(__name__)
 
@@ -13,19 +16,24 @@ def create_observation():
 
 @app.route("/observations", methods = ["GET"])
 def observations():
-    return "Retrieve all the weather observations"
+    data = db.get_all_observations()
+    return str(data)
 
 @app.route("/observations/<int:observation_id>", methods = ["GET"])
 def observation(observation_id):
-    return f"Retrieve the weather observation with ID: {observation_id}"
+    data = db.get_observation_by_id(observation_id)
+    return str(data)
 
 @app.route("/observations/<int:observation_id>", methods = ["PUT"])
 def edit_observation(observation_id):
-    return f"Update the weather observation with ID: {observation_id}"
+    db.update(observation_id, 59.33, 18.06)
+    return f"Updated ID: {observation_id}"    
 
 @app.route("/observations/<int:observation_id>", methods = ["DELETE"])
 def delete_observation(observation_id):
-    return f"Delete the weather observation with ID: {observation_id}"
+    db.delete_observation_by_id(observation_id)
+    return f"Deleted ID: {observation_id}"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
