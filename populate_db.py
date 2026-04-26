38d13485a2c2d88cc33dd19a794a72efb895a169
annalena-project <aschaeffer@northpark.edu>
2026-04-26 18:47:53 -0500

    Updated comments

--- populate_db.py
+++ populate_db.py
@@ -1,7 +1,7 @@
-# Fetches live weather data for 10 cities and saves it to the database
+# populate_db.py - Fetches live weather data for 10 cities and saves it to the database
 
 import requests                                               # To call the weather API
-from database_manager import DatabaseManager, WeatherReport   # Database tools
+from database_manager import DatabaseManager, WeatherReport   # Import database tools from database_manager.py
 
 #---------------------- List of Cities ----------------------------------------
 # List of cities I want to add to the database
@@ -24,14 +24,14 @@ cities = [
 db = DatabaseManager()      
 
 for city, country in cities:
-    # Find the coordinates for each city //  Call API to find latitude and longitude for the city
+    #  Use the geocoding API to convert the city name into latitude and longitude
     geo = requests.get( "https://geocoding-api.open-meteo.com/v1/search",params={"name": city, "count": 1}).json()["results"][0]
    
-
+    # The API returns a list of results – take the first one and save the coordinates
     latitude = geo["latitude"]
     longitude = geo["longitude"]
 
-    # Call weather API to get current weather
+    # Use the coordinates to call weather API to get current weather
     weather = requests.get("https://api.open-meteo.com/v1/forecast", params={"latitude": latitude, "longitude": longitude, "current_weather": "true"}).json()["current_weather"]
 
     # Create a WeatherReport object with all the data
@@ -56,28 +56,26 @@ data = db.get_all_observations()
 for row in data:
     print(f"ID: {row[0]}, City: {row[1]}, Temp: {row[5]}") 
 
-# Get one observation by ID number 1
-print(db.get_observation_by_id(1)) 
-
-
 
 #--------------------- COMMENTED OUT ----------------------------------------------------
 # These lines are kept here for testing purposes
+
+# Get one observation by ID number 1
+# print(db.get_observation_by_id(1)) 
 
 #db.update(1, 59.33, 18.06)         # Update latitude & longitude for ID number 1
 #print(db.get_observation_by_id(1)) # Checking updated observation
 
 # db.delete_observation_by_id(1)      # Delete observation with ID number 1
 
-#----------------------------------------------------------------------------------------
-
-
 # Get all observations again 
-data = db.get_all_observations()    
+# data = db.get_all_observations()    
 
-# Print all observations
-for row in data:
-     print(f"ID: {row[0]}, City: {row[1]}, Temp: {row[5]}")  
+# # Print all observations
+# for row in data:
+#      print(f"ID: {row[0]}, City: {row[1]}, Temp: {row[5]}")  
+
+#----------------------------------------------------------------------------------------
 
 db.con.close()      # Close database connection 
 
--- routes.py
+++ routes.py
@@ -1,13 +1,13 @@
 
 # This file runs the web app and handles all the page routes
 
-# Import what we need from Flask to build the app
+# Import what needed from Flask to build the app
 from flask import Flask, render_template, redirect,  url_for, abort, request
-# Used to call the weather API
-import requests
+import requests # Used to call the weather API
 
-# Import database functions and the WeatherReport object
+# Import DatabaseManager and WeatherReport from database_manager.py
 from database_manager import DatabaseManager, WeatherReport
+
 # Create connection to the database
 db = DatabaseManager() 
 
@@ -15,18 +15,20 @@ db = DatabaseManager()
 app = Flask(__name__)  
 
 #------------------- Home Page ----------------------------------------------------------------
+# Shows the start page (index.html)
 @app.route("/")
 def home():
     return render_template("index.html")
 
 #----------------- Create New Observation  ----------------------------------------------------
+# POST /ingest – gets city and country from the search box, fetches live weather and saves it to the database
 @app.route("/ingest", methods = ["POST"])
 def create_observation():
-    # Get city and country from the form on the home page
+    # Get city and country that user typed in on home page (index.html)
     city = request.form.get("city")
     country = request.form.get("country")
 
-    # Search for the city coordinates using Open-Meteo
+    # Use the geocoding API to convert the city name into latitude and longitude
     search_query = f"{city}, {country}"
     geo_response = requests.get("https://geocoding-api.open-meteo.com/v1/search",
     params={"name": search_query, "count": 1}).json()
@@ -35,54 +37,64 @@ def create_observation():
     if "results" not in geo_response:
         abort(404) 
 
+    # The API returns a list of results, take the first result [0] and save the coordinates (latitude and longitude)
     geo = geo_response["results"][0]
     latitude = geo["latitude"]
     longitude = geo["longitude"]
 
-    # Use the coordinates to get live weather data
+    # Use the coordinates to call weather API and get live weather data
     weather = requests.get("https://api.open-meteo.com/v1/forecast",
         params={"latitude": latitude, "longitude": longitude, "current_weather": "true"}).json()["current_weather"]
 
-    # Save everything to the database
+    # Create a WeatherReport object and save it to database 
     report = WeatherReport(city, country, latitude, longitude, weather["temperature"], 0, weather["windspeed"], weather["time"])
     db.insert_observation(report)
 
-    # Show result on home page
+    # Send back weather data snd show result on home page (index.html)
     return render_template("index.html", weather=report)
 
 #------------------- Show All Observations ----------------------------------------------------------
+# Get /observations - get all observations from the database and shows on observation.html
 @app.route("/observations", methods = ["GET"])
 def observations():
     data = db.get_all_observations()
     return render_template("observations.html", data=data)
 
 #------------------- Show One Observation -----------------------------------------------------------
+# GET /observations/<id> – fetches one specific observation by ID number and shows it in show_observation.html
 @app.route("/observations/<int:observation_id>", methods=["GET"])
 def show_observation(observation_id):
     # Look up one specific observation by ID number 
     data = db.get_observation_by_id(observation_id)
-    # Non-existent ID number, show a 404 error
+    # If ID number dont exist, show a 404 error
     if not data:
         abort(404)
     return render_template("show_observation.html", observation=data)
 
 #------------------------ Edit / Add Note ----------------------------------------------------
+# GET /observations/<id>/edit – shows the observation and lets the user write or change a note
+# POST /observations/<id>/edit – saves the note to the database
 @app.route("/observations/<int:observation_id>/edit", methods=["GET", "POST"])
 def edit_observation(observation_id):
     data = db.get_observation_by_id(observation_id)
+
+    # If ID number dont exist, show a 404 error
     if not data:
         abort(404)
-    
+
     if request.method == "POST":
-        # Save the note that was typed in the form  
+        # Get the note that was typed or edit 
         notes = request.form.get("notes")
+        # Save note to database
         db.update_notes(observation_id, notes)
-        return redirect(url_for("show_observation", observation_id=observation_id))
+        # After saving the note, send the user back to the observations page (observations.html)
+        return redirect(url_for("observations"))
+    else:
+        return render_template("show_observation.html", observation=data)
     
-    # Show the edit form 
-    return render_template("edit_observation.html", observation=data)
 
 #------------------------ Delete Observation ----------------------------------------------
+# POST /observations/<id>/delete – deletes the observation from the database 
 @app.route("/observations/<int:id>/delete", methods=["POST"])
 def delete_observation(id):
     db.delete_observation_by_id(id)
